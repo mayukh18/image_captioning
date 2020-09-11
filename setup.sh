5c8104da7cfd8b249193de884f1f35487ad872d5
Mayukh Bhattacharyya <mayukh.superb@gmail.com>
2020-09-12 01:25:05 +0530

    spot the diff add

--- setup.sh
+++ setup.sh
@@ -4,4 +4,9 @@ wget https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_t
 unzip Flickr8k_Dataset.zip
 unzip Flickr8k_text.zip
 
-python setup.py
+
+gdown https://drive.google.com/uc?id=1OVb4_3Uec_xbyUk90aWC6LFpKsIOtR7v
+wget https://raw.githubusercontent.com/harsh19/spot-the-diff/master/data/annotations/train.json
+wget https://raw.githubusercontent.com/harsh19/spot-the-diff/master/data/annotations/test.json
+wget https://raw.githubusercontent.com/harsh19/spot-the-diff/master/data/annotations/val.json
+
